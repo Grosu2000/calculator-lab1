67cc93a069465fed557f2021212ae01b0faf4184
Hrosu Oleksandr <sasha.grosu4@gmail.com>
2026-09-15 09:01:36 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "operations.h"
+
+using namespace std;
+
+const string VERSION = "v1.0.0";
+
+void printMenu() {
+    cout << "=== Calculator " << VERSION << " ===\n";
+    cout << "1. Addition\n";
+    cout << "2. Subtraction\n";
+    cout << "3. Multiplication\n";
+    cout << "4. Division\n";
+    cout << "5. Power\n";
+    cout << "6. Modulo\n";
+    cout << "0. Exit\n";
+}
+
+int main() {
+    cout << "Welcome to the calculator!\n";
+    printMenu();
+    return 0;
+}
---
+++ operations.h
@@ -0,0 +1,11 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+double op_add(double a, double b);
+double op_sub(double a, double b);
+double op_mul(double a, double b);
+double op_div(double a, double b);
+double op_pow(double a, double b);
+double op_mod(double a, double b);
+
+#endif
